f60997adbf9bafce122a200df30975bc787ac51a
Megan Byers <megan@designelixir.studio>
2024-12-03 21:30:22 -0700

    build out ugc content

--- scripts/marquee.js
+++ scripts/marquee.js
@@ -9,46 +9,132 @@ class Marquee extends HTMLElement {
     
 
     this.innerHTML = `
-    <div class="marquee fade-in-after flex-center-start" onclick="controls('marquee')">
-                    <div class="marquee-content flex-center-start">
-                        <ul class="flex-center-start">
-                            <li>Social Media Strategy</li>
-                            <li>Content Planning</li>
-                            <li>Content Creation</li>
-                            
-                            <li>User Generated Content</li>
-                            
-                            <li>Influencer Relations</li>
-                        </ul>
-                        <ul class="flex-center-start">
-                            <li>Social Media Strategy</li>
-                            <li>Content Planning</li>
-                            <li>Content Creation</li>
-                            
-                            <li>User Generated Content</li>
-                            
-                            <li>Influencer Relations</li>
-                        </ul>
-                         <ul class="flex-center-start">
-                            <li>Social Media Strategy</li>
-                            <li>Content Planning</li>
-                            <li>Content Creation</li>
-                            
-                            <li>User Generated Content</li>
-                            
-                            <li>Influencer Relations</li>
-                        </ul>
-                         <ul class="flex-center-start">
-                            <li>Social Media Strategy</li>
-                            <li>Content Planning</li>
-                            <li>Content Creation</li>
-                            
-                            <li>User Generated Content</li>
-                            
-                            <li>Influencer Relations</li>
-                        </ul>
-                    </div>
-                </div>
+    <div class="marquee flex-start-start silver">
+        <div class="marquee-track">
+            <div class="marquee-content">
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            <p>Social Media Strategy</p>
+            <p>Content Planning</p>
+            <p>Content Creation</p>
+            <p>User Generated Content</p>
+            <p>Influencer Relations</p>
+            </div>
+        </div>
+        </div>
     `;
 
   }
@@ -56,16 +142,8 @@ class Marquee extends HTMLElement {
 
 
 
+  
+
+
 // Define the custom element
 customElements.define('marquee-component', Marquee);
-document.addEventListener('DOMContentLoaded', () => {
-    const marqueeContent = document.querySelector('.marquee-content');
-    const speed = 20; // Adjust this value for faster or slower scrolling
-
-    gsap.to(marqueeContent, {
-        xPercent: -50, // Move the content left by 50% of its width
-        ease: "linear",
-        repeat: -1, // Infinite loop
-        duration: speed, // Adjust duration for speed
-    });
-});
